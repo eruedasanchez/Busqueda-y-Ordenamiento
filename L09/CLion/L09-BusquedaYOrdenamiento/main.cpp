615ad0314211c497f51573cdf751d7f50aef519c
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-07 22:58:50 -0300

    Prueba del codigo. Faltan tests

--- L09/CLion/L09-BusquedaYOrdenamiento/main.cpp
+++ L09/CLion/L09-BusquedaYOrdenamiento/main.cpp
@@ -1,7 +1,5 @@
 #include "lib/gtest.h"
 #include "src/solucion.h"
-#include <string>
-
 
 int main(int argc, char **argv) {
         /*
@@ -10,7 +8,7 @@ int main(int argc, char **argv) {
         return RUN_ALL_TESTS();
         */
 
-        /*
+        /* Ejercicio 1
         string pal1 = {'d', 'e', 'l', 'i', 'r', 'a'};
         string pal2 = {'d', 'e', 'l', 'i', 'c', 'a'};
 
@@ -23,8 +21,44 @@ int main(int argc, char **argv) {
         cout << res << endl;
          */
 
+        /* Ejercicio 2
+        string pal1 = {'d', 'e', 'l', 'i', 'r', 'a'};
+        string pal2 = {'h', 'o', 'l', 'a'};
+
+        string pal3 = {'d', 'e', 'l', 'i', 'r'};
+        string pal4 = {'h', 'o'};
+
+        vector<string> res1;
+        res1.push_back(pal1);
+        res1.push_back(pal2);
+
+        vector<string> res2;
+        res2.push_back(pal3);
+        res2.push_back(pal4);
+
+         */
+
+        // vector<vector<string>> res = { {"hola","bueno"}, {"rico","delira"} };
+        // res.push_back(res1);
+        // res.push_back(res2);
+
+       // mostrarVectorVectorString(res);
+
+       char letra = 'e';
+       int res = int(letra);
+       cout << res << endl;
+
+        //vector<string> pal1 = {"hola", "esto", "prueba", "una", "prueba"};
+
+
+        //vector<string> res = ordenarPorFrec(pal1);
+
+        //mostrarVectorString(res);
+
+
         /* Ejercicio 3.a */
 
+        /*
         vector<int> vec1 = {1, 5, 3, 2, 9};
         vector<int> vec2 = {1, 3, 4, 12};
 
@@ -48,6 +82,14 @@ int main(int argc, char **argv) {
         float med = medianaConcatenando(vec1,vec2);
         cout << " " << endl;
         cout << "Finalmente, la mediana del vector ordenado es " << med << endl;
+         */
+
+        /* vector<int> vec1 = {1, 5, 7, 9};
+        vector<int> vec2 = {1, 2, 3, 4};
+
+        vector<int> res = merge(vec1, vec2);
+
+        mostrarVectorEnteros(res); */
 
         return 0;
 }
