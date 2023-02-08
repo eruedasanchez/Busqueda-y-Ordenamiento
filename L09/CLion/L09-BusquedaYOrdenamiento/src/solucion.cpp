5a76f1f08b9d940c1514c66cc7ac4f610421cb41
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-07 22:56:18 -0300

    Resolucion ejercicios. Faltan test

--- L09/CLion/L09-BusquedaYOrdenamiento/src/solucion.cpp
+++ L09/CLion/L09-BusquedaYOrdenamiento/src/solucion.cpp
@@ -11,6 +11,33 @@ void mostrarVector(string &s){
     }
 }
 
+void mostrarVectorString(vector<string> &vs){
+    cout << "{";
+    for(int i = 0; i < vs.size(); i++){
+        if(i != vs.size()-1){
+            mostrarVector(vs[i]);
+            cout << ",";
+        } else {
+            mostrarVector(vs[i]);
+            cout << "}";
+        }
+    }
+}
+
+void mostrarVectorVectorString(vector<vector<string>> &vvs){
+    cout << "{";
+    for(int i = 0; i < vvs.size(); i++){
+        if(i != vvs.size()-1){
+            mostrarVectorString(vvs[i]);
+            cout << ",";
+        } else {
+            mostrarVectorString(vvs[i]);
+            cout << "}";
+        }
+    }
+
+}
+
 /* Ejercicio 1 */
 
 int encontrarMinPosicion(string &palabra, int desde, int hasta){
@@ -65,6 +92,32 @@ bool anagramas(string &palabra1, string &palabra2){
 }
 
 /* Ejercicio 2 */
+
+/*
+vector<string> ordenarPorFrec(vector<string> &palabras){
+    vector<string> palabrasOrdenadas;
+    vector<vector<string>> palabrasSegunTamanio;
+
+    // Recorro el vector palabras y coloco a cada una de ellas en el vector correspondiente de acuerdo a su longitud
+    for(int i = 0; i < palabras.size(); i++){
+        int longPalabraActual = palabras[i].size();
+        for(int j = 0; j <= 40; j++){
+            if(longPalabraActual == j){
+                palabrasSegunTamanio[longPalabraActual].push_back(palabras[i]);
+            }
+        }
+    }
+
+    // Ahora que las palabras estan todas agrupadas segun la longitud, las colocamos a todas en el vector palabrasOrdenadas
+    for(int k = 0; k <= 40; k++){
+        vector<string> secuPalabrasLongActual = palabrasSegunTamanio[k];
+        for(int r = 0; r < secuPalabrasLongActual.size(); r++){
+            palabrasOrdenadas.push_back(secuPalabrasLongActual[r]);
+        }
+    }
+    return palabrasOrdenadas;
+}
+*/
 
 /* Ejercicio 3 */
 
@@ -124,6 +177,109 @@ float medianaConcatenando(vector<int> &v1, vector<int> &v2){
     }
     return mediana;
 }
+
+/* Ejercicio 3.b */
+
+vector<int> merge(vector<int> &v1, vector<int> &v2){
+    vector<int> vecOrdenado;
+    int i = 0;
+    int j = 0;
+    while(i < v1.size() || j < v2.size()){
+        if(v1[i] < v2[j]){
+            vecOrdenado.push_back(v1[i]);
+            i++;
+        } else {
+            vecOrdenado.push_back(v2[j]);
+            j++;
+        }
+    }
+    /* Termino de insertar los elementos que faltan */
+    if(i == v1.size()){
+        /* Insertar los elementos restantes de v2 */
+        while(j < v2.size()){
+            vecOrdenado.push_back(v2[j]);
+            j++;
+        }
+    }
+    if(j == v2.size()){
+        /* Insertar los elementos restantes de v1 */
+        while(i < v1.size()){
+            vecOrdenado.push_back(v1[i]);
+            i++;
+        }
+    }
+    return vecOrdenado;
+}
+
+float medianaMerge(vector<int> &v1, vector<int> &v2){
+    // PRE: Estan ordenadas v1 y v2
+    vector<int> ordenados = merge(v1, v2);
+    float mediana;
+    if(ordenados.size() % 2 == 1){
+        /* Mediana en el caso impar */
+        mediana = ordenados[(ordenados.size() / 2) + 1];
+    } else {
+        /* Mediana en el caso par */
+        mediana = (ordenados[(ordenados.size() / 2)] + ordenados[(ordenados.size() / 2) + 1]) / 2;
+    }
+    return mediana;
+}
+
+/* Ejercicio 4 */
+
+/* Ejercicio 4.a */
+
+bool esMenorSegunOrdenNatural(string &palabra1, string &palabra2){
+    bool esMenor = false;
+
+    /* Defino primero si es menor utilizando el criterio por palabras */
+    int indexPal1 = 0;
+    int indexPal2 = 0;
+
+    while((indexPal1 < palabra1.size()) && (indexPal2 < palabra2.size()) && (int(palabra1[indexPal1]) == int(palabra2[indexPal2]))){
+        indexPal1++;
+        indexPal2++;
+    }
+
+    if((indexPal1 == palabra1.size()) && (indexPal2 < palabra2.size())){
+        /* Caso 1. La palabra 1 es prefijo de la palabra 2. Por lo tanto, es menor */
+        esMenor = true;
+    } else if(int(palabra1[indexPal1]) != int(palabra2[indexPal2])){
+       /* Caso donde no hay coincidencia de caracteres */
+       if(((int(palabra1[indexPal1]) > 47 && int(palabra1[indexPal1]) < 58) && !(int(palabra2[indexPal2]) > 47 && int(palabra2[indexPal2]) < 58)) || (!(int(palabra1[indexPal1]) > 47 && int(palabra1[indexPal1]) < 58) && !(int(palabra2[indexPal2]) > 47 && int(palabra2[indexPal2]) < 58) && (int(palabra1[indexPal1]) < int(palabra2[indexPal2])))){
+           /* Caso donde palabra 1 tiene un numero en el caracter no coincidente y palabra 2 tiene una letra o la letra de palabra 1 tiene un ascii menor al de palabra 2*/
+            esMenor = true;
+       } else if((int(palabra1[indexPal1]) > 47 && int(palabra1[indexPal1]) < 58) && (int(palabra2[indexPal2]) > 47 && int(palabra2[indexPal2]) < 58)){
+           /* Caso donde no coinciden los caracteres y son numeros */
+           if(int(palabra1[indexPal1]) < int(palabra2[indexPal2])){
+               esMenor = true;
+           }
+       }
+    }
+    return esMenor;
+}
+
+/* Ejercicio 4.b */
+
+void intercambiarPalabras(vector<string> &vpalabra, int indice, int minimo){
+    string tmp = vpalabra[indice];
+    vpalabra[indice] = vpalabra[minimo];
+    vpalabra[minimo] = tmp;
+}
+
+void natSorted(vector<string> &vs){
+    int min = 0;
+    for(int i = 0; i < vs.size(); i++){
+        for(int j = i+1; j < vs.size(); j++){
+            if(esMenorSegunOrdenNatural(vs[j], vs[min])){
+                min = j;
+            }
+        }
+        intercambiarPalabras(vs,i,min);
+    }
+}
+
+
 
 
 
